f777a0744a1b6032769cdc659320db2c970889d5
skboy <364971779@qq.com>
2018-10-19 15:07:44 +0800

    第一次提交

---
+++ tmp/log/cron.20181019.log.php
@@ -0,0 +1,32 @@
+<?php
+ die();
+15:02:28 task 5 executed,
+command: appName=sys&moduleName=queue&methodName=getqueue.
+return : .
+output : ok
+
+15:03:28 task 5 executed,
+command: appName=sys&moduleName=queue&methodName=getqueue.
+return : .
+output : ok
+
+15:04:28 task 5 executed,
+command: appName=sys&moduleName=queue&methodName=getqueue.
+return : .
+output : ok
+
+15:05:28 task 5 executed,
+command: appName=sys&moduleName=queue&methodName=getqueue.
+return : .
+output : ok
+
+15:06:28 task 5 executed,
+command: appName=sys&moduleName=queue&methodName=getqueue.
+return : .
+output : ok
+
+15:07:28 task 5 executed,
+command: appName=sys&moduleName=queue&methodName=getqueue.
+return : .
+output : ok
+
---
+++ tmp/log/php.20181019.log.php
@@ -0,0 +1,17 @@
+<?php
+ die();
+?>
+
+15:00:22 A session had already been started - ignoring session_start() in D:\phpStudy\PHPTutorial\WWW\ranzhi-crm\framework\base\router.class.php on line 817 when visiting 
+
+15:00:26 A session had already been started - ignoring session_start() in D:\phpStudy\PHPTutorial\WWW\ranzhi-crm\framework\base\router.class.php on line 817 when visiting 
+
+15:00:29 A session had already been started - ignoring session_start() in D:\phpStudy\PHPTutorial\WWW\ranzhi-crm\framework\base\router.class.php on line 817 when visiting 
+
+15:00:30 A session had already been started - ignoring session_start() in D:\phpStudy\PHPTutorial\WWW\ranzhi-crm\framework\base\router.class.php on line 817 when visiting 
+
+15:00:58 A session had already been started - ignoring session_start() in D:\phpStudy\PHPTutorial\WWW\ranzhi-crm\framework\base\router.class.php on line 817 when visiting 
+
+15:01:00 A session had already been started - ignoring session_start() in D:\phpStudy\PHPTutorial\WWW\ranzhi-crm\framework\base\router.class.php on line 817 when visiting 
+
+15:01:05 A session had already been started - ignoring session_start() in D:\phpStudy\PHPTutorial\WWW\ranzhi-crm\framework\base\router.class.php on line 817 when visiting 
